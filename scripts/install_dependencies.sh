56f476ca21a72f898f95ab90096aa37ff2229607
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-29 12:17:26 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -32,20 +32,18 @@ sudo rm -rf /home/ubuntu/aura-db || true
 
 sudo docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest || true 
 
-#sudo docker network rm -f aura-network || true
-
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest
 
-NETWORK_NAME="aura-network"
+# NETWORK_NAME="aura-network"
 
-# Check if the network already exists
-if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
-    echo "Network '$NETWORK_NAME' already exists."
-else
-    # Create the network if it doesn't exist
-    docker network create "$NETWORK_NAME"
-    echo "Network '$NETWORK_NAME' created."
-fi
+# # Check if the network already exists
+# if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
+#     echo "Network '$NETWORK_NAME' already exists."
+# else
+#     # Create the network if it doesn't exist
+#     docker network create "$NETWORK_NAME"
+#     echo "Network '$NETWORK_NAME' created."
+# fi
 
 
 # docker run -d --name aura-app --hostname localhost -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 80:80 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/static-website:latest
